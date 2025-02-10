853eaab1f1c5ae1353a93b215df94b74c78b9d29
VictorIgati <victor.igati@student.moringaschool.com>
2025-02-10 20:50:12 +0300

    Done

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,49 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
+    if (username == "admin" or username == "ADMIN") and password == "12345":
+        return "Access granted"
+    else:
+        return "Access denied"
     pass
-
-def hows_the_weather(temperature):
-    # your code here
-    pass
+def hows_the_weather(temp):
+    if temp <= 40:
+        return "It's brisk out there!"
+    elif temp>=40 and temp<=65:
+        return "It's a little chilly out there!"
+    elif temp>=85:
+        return "It's too dang hot out there!"
+    else:
+        return "It's perfect out there!"            
 
 def fizzbuzz(num):
-    # your code here
-    pass
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
 
-def calculator(operation, num1, num2):
-    # your code here
-    pass
+def calculator(num1, num2, op):
+    if op =='+':
+        return(num1 + num2)
+    elif op =='-':
+        return(num1 - num2)
+    elif op =='*':
+        return(num1 * num2)
+    elif op =='/':
+        return int((num1 / num2))
+        if num2 ==0:
+            return "Error: num2 cannot be divided by  0"
+    else:
+        print("Invalid operation")
+        return None
+                
+
+print(calculator(1, 1, "+"))
+print(calculator(3, 1, "-"))
+print(calculator(3, 2, "*"))
+print(calculator(4, 2, "/"))
+print(calculator("nope", 4,2))
