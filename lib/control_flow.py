fd2d3e434405a34f9a5662ba1f00d3c460e65134
VictorIgati <victor.igati@student.moringaschool.com>
2025-03-03 16:18:27 +0300

    refactor tests: simplify and consolidate test cases for control flow functions

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -6,10 +6,7 @@ def admin_login(username, password):
     else:
         return "Access denied"
 
-    # Test cases
-print(admin_login("sudo", "12345"))  # Output: "Access denied"
-print(admin_login("admin", "12345"))  # Output: "Access granted"
-print(admin_login("ADMIN", "12345"))  # Output: "Access granted"    
+    
     
 def hows_the_weather(temp):
     if temp <= 40:
@@ -20,9 +17,6 @@ def hows_the_weather(temp):
         return "It's too dang hot out there!"
     else:
         return "It's perfect out there!"            
-print(hows_the_weather(33))  # Output: "It's brisk out there!"
-print(hows_the_weather(99))  # Output: "It's too dang hot out there!"
-print(hows_the_weather(75))  # Output: "It's perfect out there!"
 def fizzbuzz(num):
     if num % 3 == 0 and num % 5 == 0:
         return "FizzBuzz"
@@ -49,11 +43,3 @@ def calculator(op, num1, num2):
     else:
         print("Invalid operation!")
         return None
-                
-
-print(calculator("+", 1, 2))
-print(calculator("-", 1, 2))
-print(calculator("*", 1, 2))
-print(calculator("/", 1, 1))
-print(calculator("/", 1, 0))
-print(calculator("nope", 4,2))
